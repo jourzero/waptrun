908845939a81a4a4d7311fcab7e42eb8d228e7bd
eric <eric@epmba.lan>
2018-04-14 16:06:25 -0400

    Minor update

--- client/js/mypac.js
+++ client/js/mypac.js
@@ -3,21 +3,21 @@ function FindProxyForURL(url, host) {
   var pacValue = "DIRECT";
 
     // If this is a test target, send to Burp
-  if ((shExpMatch(url, "http://127.0.0.1/*")) ||
-      (shExpMatch(host, "burp")) ||
-      (shExpMatch(host, "k1")) ||
-      (shExpMatch(host, "nodegoat")) ||
-      (shExpMatch(url, "*mutillidae*")) ||
-      (shExpMatch(url, "https://127.0.0.1:8443*")))
-    pacValue = "PROXY 127.0.0.1:8080";
+    if ((shExpMatch(url, "http://127.0.0.1/*")) ||
+        (shExpMatch(host, "burp")) ||
+        (shExpMatch(host, "k1")) ||
+        (shExpMatch(host, "nodegoat")) ||
+        (shExpMatch(url, "*mutillidae*")) ||
+        (shExpMatch(url, "https://127.0.0.1:8443*"))){
+            pacValue = "PROXY 127.0.0.1:8080";
+    }
 
     // If this is a swiss target, use SSH tunnel
-  else if ((shExpMatch(host, "*.ch")) || 
-           (shExpMatch(host, "waptr*"))   ||              
-           //(shExpMatch(host, "k1"))   ||              
-           (shExpMatch(host, "ssh-proxied.local")))
-    pacValue = "SOCKS 127.0.0.1:9999";
+    else if ((shExpMatch(host, "*.ch")) || 
+        (shExpMatch(host, "waptr*"))   ||              
+        (shExpMatch(host, "ssh-proxied.local"))){
+            pacValue = "SOCKS 127.0.0.1:9999";
+    }
 
-  console.log("FindProxyForURL(" + url + "," + host +") returned " + pacValue);
-  return pacValue;
+    return pacValue;
 }
