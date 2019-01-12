20305f3be5959209f9bd05fdae9bc7b0b3d111b9
Eric <eric@epmba.lan>
2019-01-11 22:21:42 -0500

    Point static to k1 system

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
 
         // Path to add to URI to show test reference details
         //'TestRefBase': "/WAPT",
-        'TestRefBase': "https://www.dropbox.com/home/static",
+        'TestRefBase': "https://k1/static",
 
         // Query for use to show a subset of available projects
         //PrjSubset    : "2016.*"
