d46938835a6b128def72fca38eb7065c79c4b342
Eric <eric@epmba.lan>
2019-04-14 15:11:09 -0400

    Add config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    port: 5000,
+    appname: "WAPT Runner",
+    //'mongodbUrl': 'mongodb://USERNAME:PASSWORD!@HOSTNAME:PORT/DBNAME',
+    mongodbUrl: "mongodb://waptrdb:27017/waptrunner",
+
+    // Base URI and suffix part (to append after the query) for searching CVEs based on a partial software name
+    //CveRptBase: "http://www.cvedetails.com/google-search-results.php?q=",
+    //CveRptBase: "https://web.nvd.nist.gov/view/vuln/search-results?query=",
+    CveRptBase:
+        "https://nvd.nist.gov/products/cpe/search/results?status=FINAL&orderBy=CPEURI&namingFormat=2.3&keyword=",
+    CveRptSuffix: "",
+
+    // Base URI for getting CWE details
+    CweUriBase: "https://cwe.mitre.org/data/definitions/",
+
+    // Base URI for gettign CVE details
+    CveUriBase: "http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=",
+
+    // Path to add to URI to show test reference details
+    TestRefBase: "https://www.wapt.me/static",
+
+    // Query for use to show a subset of available projects
+    PrjSubset: ".*"
+};
