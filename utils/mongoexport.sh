5a94aeebdf81f4c02159f6d8c25af3371e792b9e
Eric P <eric.paquet@nuance.com>
2020-11-04 15:59:26 -0500

    Change implementation to run from waptr container

--- utils/mongoexport.sh
+++ utils/mongoexport.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #========================================================================================
-# mongoexport.sh: Run mongoexport in Docker container, using MONGODB_URL from .env
+# mongoexport.sh: Run mongoexport from within the waptr container
+#
+# Prerequisite: Set MONGODB_URL env. variable if the export is from a remote DB.
 #========================================================================================
-. ../.env
-CONTAINER_NAME="waptrdb"
 COLLECTIONS[1]="testkb"
 COLLECTIONS[2]="issues"
 COLLECTIONS[3]="project"
@@ -15,20 +15,19 @@ FIELDS[3]='name,scope,scopeQry,lastTID'
 FIELDS[4]='ID,Name,Weakness_Abstraction,Status,Description_Summary,Extended_Description,Related_Weaknesses,Weakness_Ordinalities,Applicable_Platforms,Background_Details,Alternate_Terms,Modes_Of_Introduction,Exploitation_Factors,Likelihood_of_Exploit,Common_Consequences,Detection_Methods,Potential_Mitigations,Observed_Examples,Functional_Areas,Affected_Resources,Taxonomy_Mappings,Related_Attack_Patterns,Notes'
 NUM_COLLS=4
 EXPORT_TYPE="csv"
-OUTPUT_DIR=/utils/export
-DB_LOCATION="remote"
+OUTPUT_DIR=/app/data
+MONGODB_URL_LOCAL="mongodb://waptrdb:27017/waptrunner"
 
 read -p "Do you want the operation on local DB ($MONGODB_URL_LOCAL)? [y]: " answer
 if [ "$answer" = "" -o "$answer" = "y" ];then
     MONGODB_URL="$MONGODB_URL_LOCAL"
-    DB_LOCATION="local"
 fi
 
 for i in $(seq 1 $NUM_COLLS);do
-    OUT_FILENAME="$OUTPUT_DIR/${DB_LOCATION}/${COLLECTIONS[$i]}.csv"
+    OUT_FILENAME="$OUTPUT_DIR/${COLLECTIONS[$i]}.csv"
     echo -e "\n-- Exporting data from ${COLLECTIONS[$i]} collection to $OUT_FILENAME..."
-    docker exec -it "$CONTAINER_NAME" mkdir -p "$OUTPUT_DIR/${DB_LOCATION}" 2>/dev/null
-    docker exec -it "$CONTAINER_NAME" /usr/bin/mongoexport \
+    mkdir -p "$OUTPUT_DIR" 2>/dev/null
+    mongoexport \
     --fields="${FIELDS[$i]}" \
     --collection="${COLLECTIONS[$i]}" \
     --out="$OUT_FILENAME" \
