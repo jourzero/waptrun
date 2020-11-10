277bc9b74dc0208b4725d39d8d2c6f31a919454d
Eric P <pcderic@gmail.com>
2020-11-10 08:11:51 -0500

    Add dbinit files and adjust backup script

--- utils/backup-container-db.sh
+++ utils/backup-container-db.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #========================================================================================
-# backup-container-db.sh: Run mongodump in the waptrdb container (where our mongoDB is).
+# backup-container-db.sh: Run mongodump in the waptrdb/mongodb container
 #========================================================================================
-CONTAINER_NAME="waptrdb"
-#BACKUP_DIR="/backup/waptrunner.$(date +%Y%m%d)"
-BACKUP_DIR="/utils/waptrunner.$(date +%Y%m%d).$$"
+BACKUP_DIR="/backup/waptrunner.$(date +%Y%m%d).$$" 
+SVC_NAME="waptrdb"
 
-read -p "Back-up MongoDB to directory ${BACKUP_DIR} [n] " answer
+read -p "Back-up MongoDB to directory ${BACKUP_DIR} in $SVC_NAME container? [n] " answer
 if [ "$answer" = y ];then
-  docker exec "$CONTAINER_NAME" mkdir $BACKUP_DIR 
-  docker exec "$CONTAINER_NAME" /usr/bin/mongodump --db=waptrunner --host 127.0.0.1:27017 -o $BACKUP_DIR
+  docker-compose exec "$SVC_NAME" mkdir $BACKUP_DIR 
+  docker-compose exec "$SVC_NAME" /usr/bin/mongodump --db=waptrunner --host 127.0.0.1:27017 -o $BACKUP_DIR
 fi
 
 read -p "View content of backup directory ${BACKUP_DIR} [n] " answer
 if [ "$answer" = y ];then
-  docker exec "$CONTAINER_NAME" ls -lR $BACKUP_DIR
+  docker-compose exec "$SVC_NAME" ls -lR $BACKUP_DIR
 fi
