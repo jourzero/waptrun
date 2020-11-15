1394bc1729a26eb773609660b917127b79ad4291
Eric P <pcderic@gmail.com>
2020-11-15 15:21:13 -0500

    Cleanup

--- utils/backup.sh
+++ utils/backup.sh
@@ -39,42 +39,27 @@ exportAll(){
 
 # Backup DB using tools in the DB container
 echo -e "\n"
-#read -p "-- Back-up MongoDB? [n] " answer
-#if [ "$answer" = y ];then
-    echo -e "\n- Creating container backup directory ${CTR_BACKUP_DIR}, just in case..."
-    docker-compose exec "$SVC_NAME" mkdir "${CTR_BACKUP_DIR}" 
+echo -e "\n- Creating container backup directory ${CTR_BACKUP_DIR}, just in case..."
+docker-compose exec "$SVC_NAME" mkdir "${CTR_BACKUP_DIR}" 
 
-    echo -e "\n- Running mongodump and saving ${DB} DB to ${CTR_BACKUP_DIR}..."
-    #read -p "Press RETURN to proceed or CTRL-C to quit"
-    docker-compose exec "$SVC_NAME" /usr/bin/mongodump --db="${DB}" --host 127.0.0.1:27017 -o "$CTR_BACKUP_DIR"
+echo -e "\n- Running mongodump and saving ${DB} DB to ${CTR_BACKUP_DIR}..."
+docker-compose exec "$SVC_NAME" /usr/bin/mongodump --db="${DB}" --host 127.0.0.1:27017 -o "$CTR_BACKUP_DIR"
 
-    echo -e "\n- Running export and saving ${DB} collections to ${CTR_BACKUP_DIR}..."
-    #read -p "Press RETURN to proceed or CTRL-C to quit"
-    exportAll 
+echo -e "\n- Running export and saving ${DB} collections to ${CTR_BACKUP_DIR}..."
+exportAll 
 
-    echo -e "\n- Compressing to ${CTR_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz..."
-    #read -p "Press RETURN to proceed or CTRL-C to quit"
-    docker-compose exec "$SVC_NAME" tar cvfz "${CTR_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz" "$CTR_BACKUP_DIR"
-
-    #echo -e "\n- Cleaning out ${CTR_BACKUP_DIR}/${DB}/"
-    #read -p "Press RETURN to proceed or CTRL-C to quit"
-    #docker-compose exec "$SVC_NAME" rm -rf "${CTR_BACKUP_DIR}/${DB}/"
-#fi
+echo -e "\n- Compressing to ${CTR_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz..."
+docker-compose exec "$SVC_NAME" tar cvfz "${CTR_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz" "$CTR_BACKUP_DIR"
 
 # Push backup to remote host using SSH/SCP
 echo -e "\n"
 if [ "$REM_BACKUP_DIR" != "" ];then
-    #read -p "-- Push backup to remote host? [n] " answer
-    #if [ "$answer" = y ];then
-        echo -e "\n- Pushing ${HOST_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz to remote directory ${REM_BACKUP_DIR}..."
-        #read -p "Press RETURN to proceed or CTRL-C to quit"
-        if [ "$answer" = y ];then
-           scp -rp "${HOST_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz" "$REM_BACKUP_DIR"
-        fi
-        echo -e "\n- Move ${HOST_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz to $HOST_DOWNLOADS_DIR..."
-        #read -p "Press RETURN to proceed or CTRL-C to quit"
-        mv "${HOST_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz" "$HOST_DOWNLOADS_DIR"
-    #fi
+    echo -e "\n- Pushing ${HOST_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz to remote directory ${REM_BACKUP_DIR}..."
+    if [ "$answer" = y ];then
+        scp -rp "${HOST_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz" "$REM_BACKUP_DIR"
+    fi
+    echo -e "\n- Move ${HOST_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz to $HOST_DOWNLOADS_DIR..."
+    mv "${HOST_BACKUP_DIR}/${DB}.${TODAY}.$$.tgz" "$HOST_DOWNLOADS_DIR"
 else
     echo -e "\nNOTE: Variable REM_BACKUP_DIR is not defined, skipping backup to remote host."
 fi
