508619e6989e2d835fd2ce345bb02b3281310dba
Eric P <eric.paquet@nuance.com>
2020-12-14 13:20:44 -0500

    Minor update

--- utils/hacktools_shell.sh
+++ utils/hacktools_shell.sh
@@ -2,6 +2,6 @@
 #========================================================================================
 # hacktool_shell.sh: Run shell in the hacktools service.
 #========================================================================================
-SVC_NAME="hacktools"
+SVC_NAME="hack-tools"
 
 docker-compose exec "$SVC_NAME" /bin/bash
