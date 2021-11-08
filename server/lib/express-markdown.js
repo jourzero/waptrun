2d44c9b4a1c7689cfe5bd873511f838ea4675e3c
Eric P <pcderic@gmail.com>
2021-11-07 21:15:59 -0500

    Adjust to marked

--- server/lib/express-markdown.js
+++ server/lib/express-markdown.js
@@ -1,57 +1,50 @@
-var fs = require('fs'),
-	path = require('path'),
-	url = require('url'),
-	markdown = require('marked');
+var fs = require("fs"),
+    path = require("path"),
+    url = require("url"),
+    markdown = require("marked");
 
 function expressMarkdown(options) {
-	if (!options)
-		throw new Error("Missing options argument");
+    if (!options) throw new Error("Missing options argument");
 
-	var dir = options.directory,
-		view = options.view,
-		variable = options.variable || 'markdown';
+    var dir = options.directory,
+        view = options.view,
+        variable = options.variable || "markdown";
 
-	if (!dir)
-		throw new Error('Missing "directory" value in options');
+    if (!dir) throw new Error('Missing "directory" value in options');
 
-	// clean up path, remove '..'
-	dir = path.resolve(dir);
+    // clean up path, remove '..'
+    dir = path.resolve(dir);
 
-	return function(req, res, next) {
-		var file = req.url.toString(),
-			fileLower = file.toLowerCase();
+    return function (req, res, next) {
+        var file = req.url.toString(),
+            fileLower = file.toLowerCase();
 
-		if (fileLower.slice(-3) !== '.md' && fileLower.slice(-9) !== '.markdown')
-			return next();
+        if (fileLower.slice(-3) !== ".md" && fileLower.slice(-9) !== ".markdown") return next();
 
-		file = dir + '/' + url.parse(file).pathname;
-		file = path.resolve(file);
+        file = dir + "/" + url.parse(file).pathname;
+        file = path.resolve(file);
 
-		// make sure the final path is in our defined directory
-		if (file.substr(0, dir.length) !== dir)
-			return res.send(400);
+        // make sure the final path is in our defined directory
+        if (file.substr(0, dir.length) !== dir) return res.send(400);
 
-		fs.exists(file, function(exists) {
-			if (!exists)
-				return next();
+        fs.exists(file, function (exists) {
+            if (!exists) return next();
 
-			fs.readFile(file, 'utf8', function(err, data) {
-				var context = {};
-				if(err)
-					return next(err);
+            fs.readFile(file, "utf8", function (err, data) {
+                var context = {};
+                if (err) return next(err);
 
-				data = markdown(data);
+                data = markdown.parse(data);
 
-				if (view) {
-					context[variable] = data;
-					res.render(view, context);
-				}
-				else {
-					res.send(data);
-				}
-			});
-		});
-	}
+                if (view) {
+                    context[variable] = data;
+                    res.render(view, context);
+                } else {
+                    res.send(data);
+                }
+            });
+        });
+    };
 }
 
 module.exports = expressMarkdown;
