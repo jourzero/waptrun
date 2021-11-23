c000f34b97750d8f0ab5f89e600d333b7671a8e0
Eric P <pcderic@gmail.com>
2021-11-22 20:35:44 -0500

    Fix CSV header to force type

--- utils/import-cwe.sh
+++ utils/import-cwe.sh
@@ -11,7 +11,7 @@ IMPORTED_CSVFILE="$DATA_DIR/cwe-data-from-mitre.csv"
 IMPORTED_CSVFILE_TOP10="$DATA_DIR/cwe-top10s.csv"
 OLD_HEADER="ID,Name,Weakness_Abstraction,Status,Description_Summary,Likelihood_of_Exploit,Causal_Nature"
 STD_HEADER="CWE-ID,Name,Weakness Abstraction,Status,Description,Extended Description,Related Weaknesses,Weakness Ordinalities,Applicable Platforms,Background Details,Alternate Terms,Modes Of Introduction,Exploitation Factors,Likelihood of Exploit,Common Consequences,Detection Methods,Potential Mitigations,Observed Examples,Functional Areas,Affected Resources,Taxonomy Mappings,Related Attack Patterns,Notes"
-NEW_HEADER="ID.0,Name,Weakness_Abstraction,Status,Description_Summary,Extended_Description,Related_Weaknesses,Weakness_Ordinalities,Applicable_Platforms,Background_Details,Alternate_Terms,Modes_Of_Introduction,Exploitation_Factors,Likelihood_of_Exploit,Common_Consequences,Detection_Methods,Potential_Mitigations,Observed_Examples,Functional_Areas,Affected_Resources,Taxonomy_Mappings,Related_Attack_Patterns,Notes"
+NEW_HEADER="ID.int32(),Name.string(),Weakness_Abstraction.string(),Status.string(),Description_Summary.string(),Extended_Description.string(),Related_Weaknesses.string(),Weakness_Ordinalities.string(),Applicable_Platforms.string(),Background_Details.string(),Alternate_Terms.string(),Modes_Of_Introduction.string(),Exploitation_Factors.string(),Likelihood_of_Exploit.string(),Common_Consequences.string(),Detection_Methods.string(),Potential_Mitigations.string(),Observed_Examples.string(),Functional_Areas.string(),Affected_Resources.string(),Taxonomy_Mappings.string(),Related_Attack_Patterns.string(),Notes.string()"
 NO_CWE_FOUND_DATA='0,"No CWE found",,,"No CWE was found to represent this issue/finding."'
 #TOP10_2013_A9='937,"Using Components with Known Vulnerabilities (Top-10 2013)",,Incomplete,"See OWASP Top 10 2013 (A9)."'
 #TOP10_2017_A9='1026,"Using Components with Known Vulnerabilities (Top-10 2017)",,Incomplete,"See OWASP Top 10 2017 (A9)."'
@@ -108,5 +108,5 @@ read -p "-- Run mongoimport for CWE list? [n] " answer
 if [ "$answer" = y ];then
   mongoimport \
   --uri="$MONGODB_URL" --collection cwe --type csv \
-  --file "$IMPORTED_CSVFILE" --headerline  --useArrayIndexFields
+  --file "$IMPORTED_CSVFILE" --headerline  --columnsHaveTypes 
 fi
