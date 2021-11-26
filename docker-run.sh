970f3f970ce3b82e1b305909e65c555b79aa2feb
Eric P <pcderic@gmail.com>
2021-11-26 13:15:01 -0500

    Restrict shares

--- docker-run.sh
+++ docker-run.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-HOST_SHARE="$PWD"
-CTR_SHARE="/app"
+
+# Configure shares between host and container
+SHARE_LIST="backup client server"
+HOST_BASE="$PWD"
+CTR_BASE="/app"
+MOUNTS=""
+MOUNTS="$MOUNTS -v ${HOST_BASE}/backup:${CTR_BASE}/backup"
+MOUNTS="$MOUNTS -v ${HOST_BASE}/client:${CTR_BASE}/client:ro"
+MOUNTS="$MOUNTS -v ${HOST_BASE}/client:${CTR_BASE}/server:ro"
 
 # Publish specific ports
 PUB=""
-PUB="$PUB -p 127.0.0.1:5000:5000"
-PUB="$PUB -p 127.0.0.1:9230:9230"
-PUB="$PUB -p 127.0.0.1:27017:27017"
+PUB="${PUB} -p 127.0.0.1:5000:5000"
+PUB="${PUB} -p 127.0.0.1:9230:9230"
+PUB="${PUB} -p 127.0.0.1:27017:27017"
+
+# Configure run options
+RUN_OPTS="-dit"
 
 # Run container, mount local dir to /app, name it with the directory name
 set -x
-docker run -it $PUB --mount "type=bind,source=${HOST_SHARE},target=${CTR_SHARE}" $OPTS --name ${PWD##*/} ${PWD##*/} 2>&1
+docker run ${RUN_OPTS} ${PUB} ${MOUNTS} --name ${PWD##*/} ${PWD##*/} 2>&1
