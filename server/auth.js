669b889f8af9a5cd5f6ce34648a9dcbce8f81afd
Eric P <pcderic@gmail.com>
2021-12-04 10:24:53 -0500

    Reinsert Github support

--- server/auth.js
+++ server/auth.js
@@ -1,10 +1,11 @@
 var passport = require("passport");
 var GoogleStrategy = require("passport-google-oauth20").Strategy;
+var GitHubStrategy = require("passport-github").Strategy;
 const logger = require("./lib/appLogger.js");
 
 module.exports = function () {
     // OAuth 2.0-based strategies require a `verify` function which receives the
-    // credential (`accessToken`) for accessing the Facebook API on the user's
+    // credential (`accessToken`) for accessing the authenticator API on the user's
     // behalf, along with the user's profile.  The function must invoke `cb`
     // with a user object, which will be set at `req.user` in route handlers after
     // authentication.
@@ -19,11 +20,23 @@ module.exports = function () {
                 state: true,
             },
             function (accessToken, refreshToken, profile, cb) {
-                // In this example, the user's Facebook profile is supplied as the user
-                // record.  In a production-quality application, the Facebook profile should
+                // In this example, the user's profile is supplied as the user
+                // record.  In a production-quality application, the profile should
                 // be associated with a user record in the application's database, which
                 // allows for account linking and authentication with other identity
                 // providers.
+                return cb(null, profile);
+            }
+        )
+    );
+    passport.use(
+        new GitHubStrategy(
+            {
+                clientID: process.env["GITHUB_CLIENT_ID"],
+                clientSecret: process.env["GITHUB_CLIENT_SECRET"],
+                callbackURL: process.env["GITHUB_REDIRECT_URI"],
+            },
+            function (accessToken, refreshToken, profile, cb) {
                 return cb(null, profile);
             }
         )
@@ -36,7 +49,7 @@ module.exports = function () {
     // production-quality application, this would typically be as simple as
     // supplying the user ID when serializing, and querying the user record by ID
     // from the database when deserializing.  However, due to the fact that this
-    // example does not have a database, the complete Facebook profile is serialized
+    // example does not have a database, the complete user profile is serialized
     // and deserialized.
     passport.serializeUser(function (user, cb) {
         cb(null, user);
