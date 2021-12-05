2b9254913558ed1e9f6c8cadd471a188490b0fe7
Eric P <pcderic@gmail.com>
2021-12-05 02:22:30 -0500

    Fix utils mount

--- start.sh
+++ start.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Configure shares between host and container
+# Configure shares between host and container 
 HOST_BASE="$PWD"
 CTR_BASE="/app"
 MOUNTS=""
+# Writable
 MOUNTS="$MOUNTS -v ${HOST_BASE}/backup:${CTR_BASE}/backup"
 MOUNTS="$MOUNTS -v ${HOST_BASE}/.env:${CTR_BASE}/.env"
 MOUNTS="$MOUNTS -v ${HOST_BASE}/package.json:${CTR_BASE}/package.json"
 MOUNTS="$MOUNTS -v ${HOST_BASE}/package-lock.json:${CTR_BASE}/package-lock.json"
+# Read-only
 MOUNTS="$MOUNTS -v ${HOST_BASE}/client:${CTR_BASE}/client:ro"
 MOUNTS="$MOUNTS -v ${HOST_BASE}/data:${CTR_BASE}/data:ro"
 MOUNTS="$MOUNTS -v ${HOST_BASE}/waptrun-static:${CTR_BASE}/waptrun-static:ro"
-# Make server writable while working on sequelize modeling
-#MOUNTS="$MOUNTS -v ${HOST_BASE}/server:${CTR_BASE}/server:ro"
-MOUNTS="$MOUNTS -v ${HOST_BASE}/server:${CTR_BASE}/server"
+MOUNTS="$MOUNTS -v ${HOST_BASE}/server:${CTR_BASE}/server:ro"
+MOUNTS="$MOUNTS -v ${HOST_BASE}/utils:${CTR_BASE}/utils:ro"
 
 # Publish specific ports
 PUB=""
@@ -21,7 +22,7 @@ PUB="${PUB} -p 127.0.0.1:5000:5000"
 PUB="${PUB} -p 127.0.0.1:9230:9230"
 PUB="${PUB} -p 127.0.0.1:27017:27017"
 
-# Configure run options
+### Configure run options 
 #RUN_OPTS="-it"
 RUN_OPTS="-d"
 
