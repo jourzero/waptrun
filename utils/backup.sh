17b7a7e7d135d984d9c7ca6f7f73e3b832f8384b
Eric P <pcderic@gmail.com>
2021-12-05 02:44:56 -0500

    Save the DB data file too

--- utils/backup.sh
+++ utils/backup.sh
@@ -4,13 +4,19 @@
 #========================================================================================
 NOW="$(date +%Y%m%d-%H%M%S)"
 DB_DIR="/app/data"
-DB_FILE="${DB_DIR}/waptrun.sqlite3"
+DB_FILE="waptrun.sqlite3"
 BACKUP_DIR="/app/backup"
 BACKUP_FILE="waptrun.sql"
 
 cd "${BACKUP_DIR}"
 echo -e "\n- Running sqlite dump and saving to ${BACKUP_FILE}..."
-/usr/bin/sqlite3 "${DB_FILE}" .dump > "${BACKUP_FILE}"
+/usr/bin/sqlite3 "${DB_DIR}/${DB_FILE}" .dump > "${BACKUP_FILE}"
+
+echo -e "\n- Copying sqlite data file to ${BACKUP_DIR}/${DB_FILE}..."
+cp -p "${DB_DIR}/${DB_FILE}" .
+
+echo -e "\n- Compressing to ${DB_FILE}.${NOW}.tgz..."
+tar cvfz "${DB_FILE}.${NOW}.tgz" "${DB_FILE}"
 
 echo -e "\n- Compressing to ${BACKUP_FILE}.${NOW}.tgz..."
 tar cvfz "${BACKUP_FILE}.${NOW}.tgz" "${BACKUP_FILE}"
