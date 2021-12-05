f88af9557a996758b4e94a689b87feecc4b2e626
Eric P <pcderic@gmail.com>
2021-12-05 02:19:17 -0500

    Cleanup

--- server/lib/appLogger.js
+++ server/lib/appLogger.js
@@ -4,10 +4,7 @@ const {combine, timestamp, printf} = format;
 //const myFormat = printf(({level, message, timestamp}) => { return `${timestamp} ${level}: ${message}`; });
 const logger = createLogger({
     //format: combine(format.colorize(), timestamp(), myFormat),
-    transports: [
-        new transports.Console(config.logging.console),
-        new transports.File(config.logging.file),
-    ],
+    transports: [new transports.Console(config.logging.console), new transports.File(config.logging.file)],
 });
 
 module.exports = logger;
