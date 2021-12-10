522db49a82ae918fcbc85d960bd618c420173f62
jourzero <pcderic@gmail.com>
2021-12-10 09:21:18 -0500

    Remove unused script

--- utils/hacktools_shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#========================================================================================
-# hacktool_shell.sh: Run shell in the hacktools service.
-#========================================================================================
-SVC_NAME="hack-tools"
-echo "WARNING: Script $0 is disabled."
-exit 0
-
-docker-compose exec "$SVC_NAME" /bin/bash
