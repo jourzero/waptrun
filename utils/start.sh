74ca6857b8e0430143e89e93a5367583bc652f98
jourzero <pcderic@gmail.com>
2021-12-10 12:53:54 -0500

    mount reactive-handlebars

--- utils/start.sh
+++ utils/start.sh
@@ -12,8 +12,9 @@ MOUNTS="$MOUNTS -v ${HOST_BASE}/data:${CTR_BASE}/data"
 # Share for live environment changes
 MOUNTS="$MOUNTS -v ${HOST_BASE}/.env:${CTR_BASE}/.env"
 
-# Read-only shares: static reference content
+# Read-only shares: static content
 MOUNTS="$MOUNTS -v ${HOST_BASE}/waptrun-static:${CTR_BASE}/waptrun-static:ro"
+MOUNTS="$MOUNTS -v ${HOST_BASE}/reactive-handlebars:${CTR_BASE}/reactive-handlebars"
 
 # Writable shares for Dev
 if [ "$WAPTRUN_ENV" != PROD ];then 
@@ -21,9 +22,12 @@ if [ "$WAPTRUN_ENV" != PROD ];then
     MOUNTS="$MOUNTS -v ${HOST_BASE}/package.json:${CTR_BASE}/package.json"
     MOUNTS="$MOUNTS -v ${HOST_BASE}/package-lock.json:${CTR_BASE}/package-lock.json"
     # Allow edits from IDE and immediate effects in Dev container
+    MOUNTS="$MOUNTS -v ${HOST_BASE}/dbinit:${CTR_BASE}/dbinit"
     MOUNTS="$MOUNTS -v ${HOST_BASE}/utils:${CTR_BASE}/utils"
     MOUNTS="$MOUNTS -v ${HOST_BASE}/client:${CTR_BASE}/client"
     MOUNTS="$MOUNTS -v ${HOST_BASE}/server:${CTR_BASE}/server"
+    # Accelerate npm install and use node_modules on host for IDE code completion
+    MOUNTS="$MOUNTS -v ${HOST_BASE}/node_modules:${CTR_BASE}/node_modules"
 fi
 
 # Publish specific ports
