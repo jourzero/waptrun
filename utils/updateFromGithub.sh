7b207d7592d6d1b54e74beb0b273e098cd8fba14
jourzero <pcderic@gmail.com>
2021-12-10 13:56:13 -0500

    Dont exit(1) to avoid HTTP 500

--- utils/updateFromGithub.sh
+++ utils/updateFromGithub.sh
@@ -4,18 +4,18 @@
 #==================================================
 cd /app 2>/dev/null
 
-echo -e "\n\n-- Running $0..."
+echo -e "\n\n-- Running $0 (mode=${WAPTRUN_ENV})..."
 
 # Make sure we're in the container
 if [ "${PWD}" != "/app" ];then  
-    echo "ERROR: $0 is not running in container, exiting immediately."
-    exit 1
+    echo "ERROR: $0 is not running in container, exiting immediately." >&2
+    exit 
 fi
 
 # Make sure we're not in Dev
 if [ "${WAPTRUN_ENV}" != "PROD" ];then
-    echo "ERROR: $0 is not running in Prod, exiting immediately."
-    exit 1
+    echo "ERROR: $0 is not running in Prod, exiting immediately." >&2
+    exit 
 fi
 
  # Setup for pull only
