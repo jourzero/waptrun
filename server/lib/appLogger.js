bcd1e626744437d5b80cf6a143841230412f76f3
jourzero <pcderic@gmail.com>
2021-12-10 17:42:06 -0500

    Mini cleanup

--- server/lib/appLogger.js
+++ server/lib/appLogger.js
@@ -1,7 +1,6 @@
 const {createLogger, format, transports} = require("winston");
 const config = require("../config.js");
 const {combine, timestamp, printf} = format;
-//const myFormat = printf(({level, message, timestamp}) => { return `${timestamp} ${level}: ${message}`; });
 const logger = createLogger({
     //format: combine(format.colorize(), timestamp(), myFormat),
     transports: [new transports.Console(config.logging.console), new transports.File(config.logging.file)],
